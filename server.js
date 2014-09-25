55fe41e6164d7a552a32da2a1191a4a88c6b5cf3
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2014-09-25 17:45:45 +0200

    finishing multitenant architecture refactoring

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 var q = require('q'),
     path = require('path'),
     app = require("express")(),
-    bodyParser = require("body-parser"),
     fs = require("fs"),
     https = require("https"),
     http = require("http"),
@@ -13,9 +12,6 @@ var q = require('q'),
 var multitenancy;
 
 var startApp = function (app, config) {
-    config.httpPort = process.env.PORT || config.httpPort || 80;
-    config.httpsPort = process.env.HTTPS_PORT || config.httpsPort || 443;
-
     if (config.httpPort) {
         http.createServer(function (req, res) {
             if (req.url === '/api/warmup') {
@@ -23,7 +19,7 @@ var startApp = function (app, config) {
             }
 
             res.writeHead(302, {
-                'Location': "https://" + req.headers.host.split(':')[0] + ':' + config.httpsPort + req.url
+                'Location': "https://" + (req.headers.host ? req.headers.host.split(':')[0] : config.cookieSession.cookie.domain) + ':' + config.httpsPort + req.url
             });
             res.end();
         }).listen(config.httpPort).on('error', function (e) {
@@ -42,46 +38,28 @@ var startApp = function (app, config) {
     }).listen(config.httpsPort);
 };
 
-function findTempDirectory(config, cb) {
-    if (config.tempDirectory !== "azure") {
-        return cb();
-    }
-
-    require("azure").RoleEnvironment.getLocalResources(function (err, res) {
-        if (err)
-            return;
-        config.tempDirectory = res["temp"]["path"];
-        cb();
-    });
-}
-
 require("jsreport").bootstrapper({ rootDirectory: __dirname})
-    .createReporter(function () {   /*we dont wont any reporter right now */})
+    .createReporter(function () {   /*we dont wont any reporter right now */
+    })
     .start().then(function (bootstrapper) {
-    var sessions = require("client-sessions");
+        var sessions = require("client-sessions");
 
-    app.use(sessions({
-        cookieName: 'session',
-        cookie: bootstrapper.config.cookieSession.cookie,
-        secret: bootstrapper.config.cookieSession.secret,
-        duration: 1000 * 60 * 60 * 24 * 365 * 10 // forever
-    }));
+        app.use(sessions({
+            cookieName: 'session',
+            cookie: bootstrapper.config.cookieSession.cookie,
+            secret: bootstrapper.config.cookieSession.secret,
+            duration: 1000 * 60 * 60 * 24 * 365 * 10 // forever
+        }));
 
-    app.use(bodyParser.urlencoded({ extended: true, limit: "2mb"}));
-    app.use(bodyParser.json({
-        limit: "2mb"
-    }));
+        multitenancy = Multitenancy(app, bootstrapper.config);
 
-    multitenancy = Multitenancy(app, bootstrapper.config);
-    routes(app, bootstrapper.config, multitenancy);
+        routes(app, bootstrapper.config, multitenancy);
 
-    findTempDirectory(bootstrapper.config, function() {
         startApp(app, bootstrapper.config);
+    }).catch(function (e) {
+        fs.writeFileSync("startup-error.txt", e.stack);
+        console.log(e.stack);
     });
-}).catch(function (e) {
-    fs.writeFileSync("startup-error.txt", e.stack);
-    console.log(e.stack);
-});
 
 
 
